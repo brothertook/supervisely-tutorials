6dd5280fbc844b90671f416a3e91a67ab9fa1480
brothertook <brothertook@gmail.com>
2017-12-19 14:57:28 +0300

    Expose jupyter notebook

--- anpr_ocr/docker/run.sh
+++ anpr_ocr/docker/run.sh
@@ -4,4 +4,6 @@ nvidia-docker run --rm -it \
 			--net=host \
 			-v `pwd`/../src:/workdir/src \
 			-v `pwd`/../data:/data \
+			-p 8888:8888 \
 			plate-number-recognition bash
+# jupyter notebook --port=8888 --no-browser --ip=0.0.0.0
